f75e0df108e1f5f809faa6653b2e16489c587183
Tahir Ahmad <tahirahmad0234@gmail.com>
2022-09-14 23:12:12 +0600

    api link change

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -104,7 +104,7 @@ export default function Login() {
                 disabled={isLoading}
                 className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500"
               >
-                Sign in
+                {isLoading ? "Loading..." : "Sign in"}
               </button>
             </div>
 
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -162,7 +162,7 @@ export default function Register() {
                 className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500"
               >
                 <span className="absolute left-0 inset-y-0 flex items-center pl-3"></span>
-                Sign up
+                {isLoading ? "loading...." : "Sign up"}
               </button>
             </div>
           </form>
